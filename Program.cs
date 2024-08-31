60735cddfba600066048f44966c2f58fa27fced4
Md zahid hasan tanim <150315114+tanimit@users.noreply.github.com>
2024-09-01 02:54:44 +0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,29 +1,40 @@
 ﻿using System;
+using System.Runtime.InteropServices.Marshalling;
 
 public class HelloWorld
 {
     public static void Main(string[] args)
     {
-     int result=90;
-
-if(result>=80 || result==100){
-
-        Console.WriteLine("A+");
-}else if(result>=70){
-     Console.WriteLine("A");
-}else if(result>=60){
-     Console.WriteLine("A-");
-}else if(result>=50){
-     Console.WriteLine("B");
-}
-else if(result>=40){
-     Console.WriteLine("c");
-}else if(result>=33){
-     Console.WriteLine("D");
-}else{
-    Console.WriteLine("you are fail");
-}
-
+        Console.Write("Enter your result :");
+         int result=int.Parse(Console.ReadLine());
+         if(result>=80)
+         {
+            Console.WriteLine("A+");
+         }
+         else if(result>=70)
+         {
+         Console.WriteLine("A-");
+         }
+         else if(result>=60)
+         {
+            Console.WriteLine("B");
+         }
+         else if(result>=50)
+         {
+            Console.WriteLine("B-");
+         }
+         else if(result>=40)
+        {
+            Console.WriteLine("C");
+        }
+         else if(result>=33)
+         {
+           Console.WriteLine("D");
+         }
+         else
+         {
+            Console.WriteLine("Fail");
+         }
 
     }
 }
