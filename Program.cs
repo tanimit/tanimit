833bdf4e9e8925e7c91d9950ba91a6458fb579e6
Md zahid hasan tanim <150315114+tanimit@users.noreply.github.com>
2024-09-01 02:53:57 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+     int result=90;
+
+if(result>=80 || result==100){
+
+        Console.WriteLine("A+");
+}else if(result>=70){
+     Console.WriteLine("A");
+}else if(result>=60){
+     Console.WriteLine("A-");
+}else if(result>=50){
+     Console.WriteLine("B");
+}
+else if(result>=40){
+     Console.WriteLine("c");
+}else if(result>=33){
+     Console.WriteLine("D");
+}else{
+    Console.WriteLine("you are fail");
+}
+
+
+    }
+}
