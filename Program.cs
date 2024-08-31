166a4ed16a303fc2169d8459f35804bdb4ecc64c
Md zahid hasan tanim <150315114+tanimit@users.noreply.github.com>
2024-09-01 02:56:14 +0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,43 +1,26 @@
-﻿// Online C# Editor for free
-// Write, Edit and Run your C# code using C# Online Compiler
-
-using System;
+﻿using System;
 
 public class HelloWorld
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Random random = new Random();
-        int numberToGuess = random.Next(1, 101); 
-        int userGuess = 0;
-        int attempts = 0;
-        Console.WriteLine("Welcome to the Guess the Number game!");
-        Console.WriteLine("I'm thinking of a number between 1 and 100. Can you guess what it is?");
-        while (userGuess != numberToGuess)
+        // Initialization of variable i
+        int i;
+        for (i = 1; i < 10; i++)
         {
-            Console.Write("Enter your guess: ");
-            string input = Console.ReadLine();
-            if (int.TryParse(input, out userGuess))
-            {
-                attempts++;
-
-                if (userGuess < numberToGuess)
-                {
-                    Console.WriteLine("Too low! Try again.");
-                }
-                else if (userGuess > numberToGuess)
-                {
-                    Console.WriteLine("Too high! Try again.");
-                }
-                else
-                {
-                    Console.WriteLine($"Congratulations! You guessed the number in {attempts} attempts.");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Invalid input. Please enter a valid number.");
-            }
+           Console.WriteLine(i);
         }
+         i=1;
+        while (i<10)
+        {
+            
+        Console.WriteLine(i);
+        i++;
+        }
+        // do{
+        //     Console.WriteLine(i);
+        //     i++;
+        // }while(i<10);
+        // Print the message to the console after the loop finishes
     }
 }
